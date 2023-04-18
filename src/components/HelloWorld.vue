78148ad751ed28d62c9ea55fde9076499c98f184
WaleyChAn <zerogeneral@foxmail.com>
2023-04-18 17:11:43 +0800

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,18 @@
+<template>
+  <iframe id="fd-jspdf" name="fd-jspdf" :src="`/static/pdf/web/viewer.html?file=${pdfUrl}`" style="width:100%;height:100vh;" frameborder="0"></iframe>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      pdfUrl: 'http://localhost:8080/static/test.pdf'
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
